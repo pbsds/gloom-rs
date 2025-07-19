d27d325b109bbb95c0eb1a973501b0f3eda0871b
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-07-20 01:15:51 +0200

    Pandoc for windows

--- create_code_archive_for_blackboard_WINDOWS.bat
+++ create_code_archive_for_blackboard_WINDOWS.bat
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 vendor/7za.exe a source.zip src shaders Cargo.lock Cargo.toml
+=======
+vendor\7za.exe a source.zip src shaders Cargo.lock Cargo.toml
+>>>>>>> 8b39e25 (Fix windows delivery script)
 pause
